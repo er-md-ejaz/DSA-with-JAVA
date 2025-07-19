67459956f323548f096d830b2df16a1d9b29e6e2
er-md-ejaz <180041671+er-md-ejaz@users.noreply.github.com>
2025-07-19 12:57:53 +0530

    rotated sorted array and it's variant

--- DSA/BinarySearch/RotatedSortedArray.java
+++ DSA/BinarySearch/RotatedSortedArray.java
@@ -2,10 +2,66 @@
 
 public class RotatedSortedArray {
     public static void main(String[] args) {
-        // int arr[] = {4, 5, 6, 7, 0, 1, 2};
+        int arr[] = {4, 5, 6, 0, 1, 1, 2};
         int arr2[] = {};
-        System.out.println(findInRotatedArray(arr2));
-        
+        // System.out.println(findInRotatedArray(arr2));
+        // minElemInRotatedSortedArray(arr);
+        noOfRotations(arr);
+    }
+
+    public static void minElemInRotatedSortedArray(int arr[]){
+        int start = 0;
+        int end = arr.length-1;
+        int minElem = Integer.MAX_VALUE;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (arr[start] == arr[mid] && arr[mid] == arr[end]) {
+                start++;
+                end--;
+            }
+
+            if (arr[start] < arr[mid]) {
+                minElem = Math.min(arr[start], minElem);
+                start = mid+1;
+            }
+            else {
+                minElem = Math.min(arr[mid], minElem);
+                end = mid-1;
+            }
+        }
+        System.out.println(minElem);
+    }
+
+    public static void noOfRotations(int arr[]){
+        int start = 0;
+        int end = arr.length-1;
+        int minElem = Integer.MAX_VALUE;
+        int minIdx = -1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (arr[start] == arr[mid] && arr[mid] == arr[end]) {
+                start++;
+                end--;
+            }
+
+            if (arr[start] < arr[mid]) {
+                if (arr[start] < minElem) {
+                    minElem = Math.min(arr[start], minElem);
+                    minIdx = start;
+                }
+                start = mid+1;
+            }
+            else {
+                if (arr[mid] < minElem) {
+                    minElem = Math.min(arr[mid], minElem);
+                    minIdx = mid;
+                }
+                end = mid-1;
+            }
+        }
+        System.out.println(minIdx);
     }
 
     public static int findInRotatedArray(int arr[]){
